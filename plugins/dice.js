69bd8d31696b0643f146f331efa01d0bfeb58fb9
DentaCH <denta123.ft@gmail.com>
2022-05-16 00:48:11 +0800

    main

---
+++ plugins/dice.js
@@ -0,0 +1,56 @@
+const fs = require('fs')
+
+const command = async (data) => {
+    try {
+         let random = Math.floor(Math.random() * 6)
+         let dice = fs.readFileSync(dir.assets + random + '.webp')
+         
+         client.sendMessage(data.from, { sticker: dice }, { quoted: data.chat })
+    } catch (e) {
+        logger.error(e)
+        client.sendMessage(data.from, {
+            text: util.format(e)
+        }, {
+            quoted: data.chat
+        })
+    }
+};
+//PERMISSION
+command.permission = {
+    owner: false,
+    admin: {
+        bot: false,
+        normal: false,
+        super: false
+    },
+    premium: false,
+    group: false,
+    private: false
+};
+//NEED
+command.need = {
+    register: false,
+    limit: {
+        amount: 0
+    },
+    cash: {
+        amount: 0
+    },
+    level: 0
+};
+//INFO
+command.name = 'Dice';
+command.help = ['dice'].map(v => '');
+command.tags = ['games'];
+command.use = (/^dice$/i);
+
+//OPTION
+command.disable = false;
+command.beta = false;
+command.support = {
+    android: true,
+    linux: true,
+    windows: true
+};
+
+module.exports = command;
