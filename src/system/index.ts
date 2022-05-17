b7a285bf434000d81c73178008edc7cdd5cba911
DentaCH <denta123.ft@gmail.com>
2022-05-18 03:42:27 +0800

    ts

---
+++ src/system/index.ts
@@ -0,0 +1,33 @@
+import makeWASocket, { useSingleFileAuthState, BufferJSON, DEFAULT_CONNECTION_CONFIG } from "@adiwajshing/baileys"
+import * as fs from "fs"
+
+let pathAuth = '../dxtstd-bot/auth.json'
+
+const loadAuth = function (file: string) {
+    const { state } = useSingleFileAuthState(file)
+    return state
+}
+
+const saveAuth =  function (auth: Object, file: string) {
+    file = file || "./auth.json"
+    fs.writeFileSync(file, JSON.stringify(auth, BufferJSON.replacer))
+}
+
+const startClient = function () {
+    const opts = {
+        auth: loadAuth(pathAuth),
+        printQRInTerminal: true,
+        version: DEFAULT_CONNECTION_CONFIG.version
+    } as any
+    return makeWASocket(opts)
+}
+
+const start = async function () {
+    const client = global.client = startClient()
+    client.ev.on('creds.update', () => {
+        saveAuth(client.authState, pathAuth)
+    })
+    return client
+}
+
+start()
