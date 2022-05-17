e9a9b74247c24e271e5a87825801dab9476c877c
DentaCH <denta123.ft@gmail.com>
2022-05-18 04:42:54 +0800

    ts

---
+++ lib/logger.ts
@@ -0,0 +1,70 @@
+import pino from "pino"
+import chalk from "chalk"
+import moment from 'moment-timezone'
+
+const coloringText = function (text: string, color: string) {
+    return !color ? chalk.keyword('white')(text) : chalk.keyword(color)(text)
+}
+
+const coloringBGText = function (text: string, color: string) {
+    return !color ? chalk.bgKeyword('white')(text) : chalk.bgKeyword(color)(text)
+}
+
+const colors = ['red', 'white', 'black', 'blue', 'yellow', 'green', 'aqua', 'cyan']
+
+//LOGGER
+const pinoLevel = function (logLevel) {
+    if (logLevel == 31) {
+        return coloringText("[  " + "MESSAGE" + "  ]", 'cyan')
+    } else if (logLevel == 32) {
+        return coloringText("[  " + "COMMAND" + "  ]", 'cyan')
+    }
+
+    if (logLevel == 10) {
+        return coloringText(("[  " + "TRACE" + "  ]"), 'yellow')
+    } else if (logLevel == 20) {
+        return coloringText(("[  " + "DEBUG" + "  ]"), 'yellow')
+    } else if (logLevel == 30) {
+        return coloringText(("[  " + "INFO" + "  ]"), 'lime')
+    } else if (logLevel == 40) {
+        return coloringText(("[  " + "WARN" + "  ]"), 'yellow')
+    } else if (logLevel == 50) {
+        return coloringText(("[  " + "ERROR" + "  ]"), 'red')
+    } else if (logLevel == 60) {
+        return coloringBGText(("[ " + "FATAL" + "  ]"), 'red')
+    } else {
+        return coloringText(("[  " + "USERLVL" + "  ]"), 'white')
+    }
+
+
+}
+
+
+const customPino = {
+    time: timestamp => moment.tz("Asia/Makassar").format('HH:mm:ss'),
+    level: levelLog => pinoLevel(levelLog)
+}
+
+const msgFormatPino = function (logPino) {
+    //console.log(logPino)
+    let msg = logPino.msg
+    return msg
+}
+
+const PINO = pino({
+    customLevels: {
+        message: 31,
+        command: 32
+    },
+    prettyPrint: {
+        levelFirst: false,
+        colorize: true,
+        ignore: 'pid,hostname',
+        messageFormat: msgFormatPino,
+        customPrettifiers: customPino
+    }
+})
+
+export {
+    PINO as logger
+}
--- src/system/command.ts
+++ src/system/command.ts
@@ -1,5 +1,6 @@
 import * as fs from "fs"
 import * as path from "path"
+import { logger } from '../../lib/logger'
 
 const commands = []
 const file = fs.readdirSync('../cmd/').filter(filename => filename.endsWith('.ts'))
@@ -7,8 +8,10 @@ for (let i in file) {
     try { 
         let command = require(path.join(__dirname, 'src', 'cmd', file[i]))
     } catch (error) {
-        
+        logger.error(error,`Command [ ${file} ]`)
     }
 }
 
-export {}
+export {
+    commands
+}
