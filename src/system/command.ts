6e6163c2b6089b8101a55ba1a900d4dd0037ac96
DentaCH <denta123.ft@gmail.com>
2022-05-21 02:16:37 +0800

    ts

--- src/system/command.ts
+++ src/system/command.ts
@@ -8,7 +8,7 @@ const file = fs.readdirSync('../commands/').filter((filename) => {
 });
 for (const i in file) {
     try { 
-        const command = import(path.join(__dirname, 'src', 'cmd', file[i]))
+        const command = require(path.join(__dirname, 'src', 'cmd', file[i]))
 
         commands.push(command)
     } catch (error) {
